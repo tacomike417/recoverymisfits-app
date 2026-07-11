095a0662918dfec17ea645f1dd648c7ca75b7b94
tacomike417 <mnasvadi@gmail.com>
2026-07-11 06:10:42 -0400

    Create story.js

---
+++ story.js
@@ -0,0 +1,33 @@
+{
+    id: 1,
+    title: "Just One More Time",
+
+    gameplay: {
+        duration: 30,
+        background: "new_york_street",
+        obstacles: [
+            "bottle",
+            "bar",
+            "woman",
+            "bills"
+        ],
+        collectibles: [
+            "coffee"
+        ]
+    },
+
+    cards: [
+        {
+            title: "Just One More Time",
+            text: "Bill Wilson wanted to stop drinking.\n\nHe meant it every time."
+        },
+        {
+            text: "Every morning began with another promise.\n\nBy nightfall...\n\nsomething had changed."
+        },
+        {
+            text: "He didn't know it yet...\n\nbut he wasn't fighting alcohol.\n\nHe was fighting alcoholism."
+        }
+    ],
+
+    nextChapter: 2
+}
