83ef6dbec54af9b74535d6f10b695881e7cc786d
tacomike417 <mnasvadi@gmail.com>
2026-07-11 06:11:54 -0400

    Update story.js

--- story.js
+++ story.js
@@ -1,33 +1,26 @@
-{
+const chapters = [
+  {
     id: 1,
     title: "Just One More Time",
 
     gameplay: {
-        duration: 30,
-        background: "new_york_street",
-        obstacles: [
-            "bottle",
-            "bar",
-            "woman",
-            "bills"
-        ],
-        collectibles: [
-            "coffee"
-        ]
+      duration: 30,
+      background: "street",
+      obstacles: ["bottle", "bar", "woman", "bills"],
+      collectibles: ["coffee"]
     },
 
     cards: [
-        {
-            title: "Just One More Time",
-            text: "Bill Wilson wanted to stop drinking.\n\nHe meant it every time."
-        },
-        {
-            text: "Every morning began with another promise.\n\nBy nightfall...\n\nsomething had changed."
-        },
-        {
-            text: "He didn't know it yet...\n\nbut he wasn't fighting alcohol.\n\nHe was fighting alcoholism."
-        }
-    ],
-
-    nextChapter: 2
-}
+      {
+        title: "Just One More Time",
+        text: "Bill wanted to stop drinking.\n\nHe meant it every time."
+      },
+      {
+        text: "Every morning began with another promise.\n\nBy nightfall...\n\nsomething had changed."
+      },
+      {
+        text: "He didn't know it yet...\n\nbut he wasn't fighting alcohol.\n\nHe was fighting alcoholism."
+      }
+    ]
+  }
+];
