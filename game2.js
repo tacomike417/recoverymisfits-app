27dedec26840bc3da3f55f8277e21f17cb7f8a97
Mike Nasvadi <mnasvadi@gmail.com>
2026-07-25 15:56:50 -0400

    Restore working game2 before effects extraction

--- game2.js
+++ game2.js
@@ -55,13 +55,6 @@
   const ctx = canvas.getContext("2d");
 
   const engine = window.RecoveryEngine;
-(() => {
-  "use strict";
-
-  const AudioEngine = {};
-
-  window.RecoveryAudio = AudioEngine;
-})();
 
   /*
     Chapters are numbered normally in the URL:
