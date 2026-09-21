3a47aaf29941e6085698dc47ce382f92b6027f16
Mike Nasvadi <mnasvadi@gmail.com>
2026-09-21 08:54:05 -0400

    Absolute of the Day, rotating from the 1st of each month

--- topbar.js
+++ topbar.js
@@ -20,7 +20,7 @@
      different device. Bump RM_VERSION on any deploy worth telling apart;
      tapping the badge reloads past the cache.
      ==================================================================== */
-  const RM_VERSION = "v441";
+  const RM_VERSION = "v442";
 
   const SHOW_VER = (function () {
     try {
